2786eca3dc9207eeced4de2a47b85160fb9f110d
krolandin <krolandin@gmail.com>
2023-05-01 14:18:52 +0300

    Theories

--- src/Theory/theoryTypes.py
+++ src/Theory/theoryTypes.py
@@ -1,5 +1,5 @@
 from theoryModels import TheoryTrPh_fH
-from temp.theoryLangasite_GaussDirection import TheoryLangasite_GaussDirection
+# from temp.theoryLangasite_GaussDirection import TheoryLangasite_GaussDirection
 from theoryLangasite_M_H import TheoryLangasite_M_H
 from theoryLangasite_M_teta import TheoryLangasite_M_teta
 from theoryHoLGS_DistrAngleDcf import TheoryHoLGS_DistrAngleDcf
@@ -10,7 +10,7 @@ from theoryTrPh_f import TheoryTrPh_f
 class TheoryType:
     types = {TheoryTrPh_fH.name: TheoryTrPh_fH,
              TheoryTrPh_f.name: TheoryTrPh_f,
-             TheoryLangasite_GaussDirection.name: TheoryLangasite_GaussDirection,
+             # TheoryLangasite_GaussDirection.name: TheoryLangasite_GaussDirection,
              TheoryLangasite_M_H.name: TheoryLangasite_M_H,
              TheoryLangasite_M_teta.name: TheoryLangasite_M_teta,
              TheoryHoLGS_DistrAngleDcf.name: TheoryHoLGS_DistrAngleDcf,
