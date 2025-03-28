b233c924d647826d5caeaf8410cbf399a5254dcd
work <krolandin@gmail.com>
2025-03-28 11:13:28 +0100

    Update numberLineEdit

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ from spectraPlot import SpectraPlot
 import pickle
 from spectrumObject import SpectrumObject
 from theoryUI import TheoryUI
+from experimentUI import *
 from theoryFileTree import TheoryFileTree
 from PyQt5.QtCore import pyqtSlot, Qt, QSize
 import clipboard
 from dataTypes import DataTypes, FileTypes
 from screenSettings import screenSize
-import numpy as np
+from darktheme.widget_template import DarkApplication, DarkPalette
+from ascManager import parse_asc_data
 
 
 class MainWindow(QMainWindow):
@@ -47,16 +49,11 @@ class MainWindow(QMainWindow):
         bottomContainer = QWidget(self)
         mainSplitter.addWidget(bottomContainer)
         bottomContainer.layout = QtWidgets.QHBoxLayout(bottomContainer)
-        # bottomContainer.layout.setContentsMargins(0,0,0,0)
         self.tabs = QTabWidget()
 
-        # self.tabs.setTabBar(TabBar())
-        self.tabs.setTabsClosable(True)
-        # self.tabs.setMovable(True)
+        self.tabs.setTabsClosable(False)
         self.tabs.setDocumentMode(True)
-        # self.tabs.setElideMode(Qt.ElideRight)
         self.tabs.setUsesScrollButtons(True)
-        self.tabs.tabCloseRequested.connect(self.closeTab)
 
         bottomContainer.layout.addWidget(self.tabs)
         rightMenuContainer = QWidget(self)
@@ -66,8 +63,6 @@ class MainWindow(QMainWindow):
         # ##################### BOTTOM BLOCK #########################
 
         # ##################### UPPER BLOCK > PLOTS #########################
-        self.plotColors = [0xFF1700, 0xFF8E00, 0xFFE400, 0x06FF00, 0x23049D, 0xAA2EE6, 0xFF79CD, 0xFFDF6B]
-        self.plotColorsNum = 0
         self.plotByType = {}
         self.currentPlotWithCursor = None
         for dataType in DataTypes.types:
@@ -94,7 +89,7 @@ class MainWindow(QMainWindow):
         tabTheoryFiles.layout = QtWidgets.QHBoxLayout(tabTheoryFiles)
         self.theoryFileTree = TheoryFileTree(tabTheoryFiles.layout)
 
-        self.tabs.tabBar().setTabButton(0, QTabBar.RightSide, None)
+        self.tabs.tabBar().setTabButton(0, QTabBar.RightSide, None)  # Hide close button
 
         def onOpenTheory(filePath):
             tup = loadTheory(filePath)
@@ -111,7 +106,7 @@ class MainWindow(QMainWindow):
                     for row in range(len(table.spectra)):
                         if experiment["filePath"] == table.spectra[row].filePath and \
                                 experiment["inFileNum"] == table.spectra[row].inFileNum:
-                            self.selectLoadedSpectra(row, table)
+                            self.experimentUI.selectLoadedSpectra(row, table)
                             break
 
         self.theoryFileTree.signalOpenFiles.connect(onOpenTheory)
@@ -120,7 +115,7 @@ class MainWindow(QMainWindow):
         # ############## BOTTOM BLOCK > THEORY ###############
         tabTheory = QWidget(self)
         self.tabs.addTab(tabTheory, "Theory")
-        self.tabs.tabBar().setTabButton(1, QTabBar.RightSide, None)
+        self.tabs.tabBar().setTabButton(1, QTabBar.RightSide, None)  # Hide close button
         tabTheory.layout = QtWidgets.QHBoxLayout(tabTheory)
         theoryContainer = QWidget(self)
         tabTheory.layout.addWidget(theoryContainer)
@@ -129,67 +124,27 @@ class MainWindow(QMainWindow):
         self.theoryUI = TheoryUI(theoryContainer.layout, self.plotByType, self.tables)
         # ############## BOTTOM BLOCK > THEORY ###############
 
+        # ############## BOTTOM BLOCK > EXPERIMENT ###############
+        tabExperiment = QWidget(self)
+        self.tabs.addTab(tabExperiment, "Experiment")
+        self.tabs.tabBar().setTabButton(2, QTabBar.RightSide, None)  # Hide close button
+        tabExperiment.layout = QtWidgets.QHBoxLayout(tabExperiment)
+        experimentContainer = QWidget(self)
+        tabExperiment.layout.addWidget(experimentContainer)
+        experimentContainer.layout = QtWidgets.QHBoxLayout(experimentContainer)
+        self.experimentUI = ExperimentUI(experimentContainer.layout, self.plotByType, self.tables)
+        # ############## BOTTOM BLOCK > EXPERIMENT ###############
+
         # ##################### INIT #########################
         self.setAcceptDrops(True)
         # self.loadExpFiles(self.loadDataOnStart())
-        self.currentTableSpectrum = None
-        mainSplitter.setSizes([600, 300])
+        # self.currentTableSpectrum = None
+        mainSplitter.setSizes([900, 200])
         # ##################### INIT #########################
 
-    ################# TABLES #################
-    def addNewTable(self, fileType, newSpectra, tabName):
-        tabExp = QWidget(self)
-        self.tabs.addTab(tabExp, "[" + fileType + "] " + tabName)
-        tableWidget = QTableWidget()
-        for i in range(11):
-            tableWidget.insertColumn(i)
-        tableWidget.setHorizontalHeaderLabels(["Sample name", "Spectrum name", "Temperature", "Thickness",
-                                               "Date", "File name", "Color", "Start", "End", "Points",
-                                               "Type"])
-        tabExp.layout = QtWidgets.QHBoxLayout(tabExp)
-        tabExp.layout.addWidget(tableWidget)
-        tabExp_rt = QWidget(self)
-        tabExp.layout.addWidget(tabExp_rt)
-        tabExp_rt.setMaximumWidth(220)
-        tabExp_rt.layout = QtWidgets.QVBoxLayout(tabExp_rt)
-        tabExp_rt.layout.setAlignment(Qt.AlignTop)
-        table = SpectraTable(tableWidget, fileType, tabExp_rt.layout, self.plotByType)
-        table.spectra.extend(newSpectra)
-        table.newSpectraAdded = True
-        self.tables.append(table)
-
-        def onSignalCellClick(row, col, tableFileType):
-            self.selectRow(row, col, table)
-
-        table.signalCellClick.connect(onSignalCellClick)
-
-    def closeAllExpTabsAndTheories(self):
-        for i in range(len(self.tables)):
-            index = 1 + len(self.tables) - i
-            self.tables[i].onClearTable()
-            tab = self.tabs.widget(index)
-            tab.deleteLater()
-            self.tabs.removeTab(index)
-            self.tables[i].signalCellClick.disconnect()
-        self.tables.clear()
+    def closeAllExperimentAndTheories(self):
         self.theoryUI.theoryList.onRemoveAll()
-
-    def closeExpTable(self, index):
-        pass
-
-    @pyqtSlot(int)
-    def closeTab(self, index):
-        if index < 2:
-            return
-        tab = self.tabs.widget(index)
-        tab.deleteLater()
-        self.tabs.removeTab(index)
-        i = index - 2
-        self.tables[i].onClearTable()
-        self.tables[i].signalCellClick.disconnect()
-        self.tables.remove(self.tables[i])
-
-    ################# TABLES #################
+        self.experimentUI.experimentList.onRemoveAll()
 
     ################# PLOTS #################
     @pyqtSlot(str)
@@ -230,82 +185,10 @@ class MainWindow(QMainWindow):
                         yValue = spectrum.yValues[i]
                         if spectrum.dataType == DataTypes.Phf:
                             yValue = spectrum.yValues[i] / spectrum.xValues[i]
-                        if spectrum.dataType == DataTypes.MirrorH:
-                            yValue = -yValue
                         spectrumStr += str(xValue) + "\t" + str(yValue) + "\n"
         clipboard.copy(spectrumStr)
         self.statusbar.showMessage("Copy experiment to clipboard: " + plotDataType)
-
     ################# PLOTS #################
-
-    ################# SELECTION #################
-    def selectRow(self, row, col, table):
-        spectrum = table.spectra[row]
-        plot = self.plotByType[spectrum.dataType]
-        table.saveCorrection()
-        if spectrum in table.selectedPlotsBySpectra:
-            if col == table.labels.index("Color"):
-                startColor = QColor(spectrum.color)  # opening color dialog
-                color = QColorDialog.getColor(startColor)
-                if not (color.isValid()):
-                    return
-                spectrum.color = int(color.name().replace("#", ""), 16)
-                table.selectedPlotsBySpectra[spectrum].setSymbolBrush(color)
-                table.setRowColor(row, True, spectrum.color)
-                return
-            # plot.clearCursor()
-            # plot.plotWidget.removeItem(table.selectedPlotsBySpectra[spectrum])
-            plot.removePlotItem(table.selectedPlotsBySpectra[spectrum])
-            del table.selectedPlotsBySpectra[spectrum]
-            table.setRowColor(row, False, 0x000000)
-            table.currentSelectedSpectrum = None
-            table.correctionWidget.setVisible(False)
-            self.currentTableSpectrum = None
-            self.checkClosePlot(plot)
-        else:
-            self.plotSpectra(spectrum, table, row, plot)
-            self.currentTableSpectrum = spectrum
-            plot.showPlotWidget()
-
-    def checkClosePlot(self, plot):
-        theories = self.theoryUI.theoryList.theories
-        for table in self.tables:
-            for spectrum in table.selectedPlotsBySpectra:
-                if spectrum.dataType == plot.dataType:
-                    return
-        for theory in theories:
-            for curveDataType in theory.curves:
-                if curveDataType == plot.dataType:
-                    return
-        plot.hidePlotWidget()
-
-    def plotSpectra(self, spectrum, table, row, plot):
-        self.setSpectrumColor(spectrum)
-        table.setRowColor(row, True, spectrum.color)
-        table.currentSelectedSpectrum = spectrum
-        table.numberEdit.resetSpectrum(spectrum)
-        table.selectedPlotsBySpectra[spectrum] = plot.plot(spectrum)
-
-    def selectLoadedSpectra(self, row, table):
-        spectrum = table.spectra[row]
-        plot = self.plotByType[spectrum.dataType]
-        table.saveCorrection()
-        if spectrum in table.selectedPlotsBySpectra:
-            pass
-        else:
-            self.plotSpectra(spectrum, table, row, plot)
-            self.currentTableSpectrum = spectrum
-            plot.showPlotWidget()
-
-    def setSpectrumColor(self, spectrum: SpectrumObject):
-        if spectrum.color:
-            return
-        spectrum.color = self.plotColors[self.plotColorsNum]
-        self.plotColorsNum += 1
-        if self.plotColorsNum > len(self.plotColors) - 1:
-            self.plotColorsNum = 0
-
-    ################# SELECTION #################
 
     ################# DRAG AND DROP FILES #################
     def dragEnterEvent(self, event):
@@ -328,31 +211,30 @@ class MainWindow(QMainWindow):
             newSpectra = readSpectraFile(f)
             if len(newSpectra) == 0:
                 return
-            self.addNewTable(newSpectra[0].fileType, newSpectra, Path(f).name)
+            self.experimentUI.addNewTable(newSpectra[0].fileType, newSpectra, Path(f).name)
             fileNames += Path(f).name + " "
             FileManager.files.append(f)
         self.statusbar.showMessage("Loaded: " + fileNames)
         for table in self.tables:
             if table.newSpectraAdded:
                 table.fillTable()
-
     ################# DRAG AND DROP FILES #################
 
+    ################# SAVE/LOAD DATA ON EXIT/START #################
     def saveDataOnExit(self):
-        pass
-        # with open('expFiles.pkl', 'wb') as file:
-        #     pickle.dump(FileManager.files, file)
+        with open('expFiles.pkl', 'wb') as file:
+            pickle.dump(FileManager.files, file)
 
     def loadDataOnStart(self):
-        pass
-        # if Path('expFiles.pkl').is_file():
-        #     with open('expFiles.pkl', 'rb') as file:
-        #         return pickle.load(file)
+        if Path('expFiles.pkl').is_file():
+            with open('expFiles.pkl', 'rb') as file:
+                return pickle.load(file)
+    ################# SAVE/LOAD DATA ON EXIT/START #################
 
-    ##################### Key pressed #####################
+    ################# KEY PRESSED #################
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
-            self.closeAllExpTabsAndTheories()
+            self.closeAllExperimentAndTheories()
         if event.key() == Qt.Key_Left:
             if self.currentPlotWithCursor:
                 self.currentPlotWithCursor.setCursorToPrev()
@@ -360,46 +242,30 @@ class MainWindow(QMainWindow):
             if self.currentPlotWithCursor:
                 self.currentPlotWithCursor.setCursorToNext()
         if event.key() == Qt.Key_Space:
-            print("Space")
-        if event.key() == Qt.Key_Q:
-            if self.theoryUI.currentTheory is not None:
-                self.theoryUI.currentTheory.update()
-                self.theoryUI.currentTheory.plotCurves()
-                if self.theoryUI.currentTheory.name == "Ho LGS DistrAngleDcf" or \
-                    self.theoryUI.currentTheory.name == "Ho LGS DistrAngleMu" or \
-                    self.theoryUI.currentTheory.name == "Ho LGS DistrAngleMuDcf":
-                    for spectrum in self.tables[0].spectra:
-                        v = spectrum.xValues
-                        maximum = np.max(v)
-                        minimum = np.min(v)
-                        index_of_maximum = np.where(v == maximum)
-                        index_of_minimum = np.where(v == minimum)
-
-                        # print(index_of_maximum[0][0])
-                        # print(index_of_minimum[0][0])
-
-                        if spectrum.dataType == DataTypes.SignalH:
-                            self.tables[0].rightClickSpectrum = self.tables[0].currentSelectedSpectrum = spectrum
-                            self.tables[0].onCorrectionYMultiply()
-                            value = self.theoryUI.currentTheory.curves[0].y[0] / spectrum.yValues[index_of_minimum[0][0]]
-                            self.tables[0].numberEdit.resetValue(value)
-                            self.tables[0].saveCorrectionForAutoShift()
-                        if spectrum.dataType == DataTypes.MirrorH:
-                            self.tables[0].rightClickSpectrum = self.tables[0].currentSelectedSpectrum = spectrum
-                            self.tables[0].onCorrectionYShift()
-                            value = - self.theoryUI.currentTheory.curves[1].y[0] - spectrum.yValues[index_of_minimum[0][0]]
-                            self.tables[0].numberEdit.setText(f"{value:.8f}")
-                        if spectrum.dataType == DataTypes.dMu_H:
-                            self.tables[0].rightClickSpectrum = self.tables[0].currentSelectedSpectrum = spectrum
-                            self.tables[0].onCorrectionYShift()
-                            value = self.theoryUI.currentTheory.curves[1].y[0]
-                            self.tables[0].numberEdit.setText(f"{value:.8f}")
-
+            pass
+            # self.copyTableSpectrumToClipboard()
         if event.key() == Qt.Key_Return:
             if self.theoryUI.currentTheory is not None:
                 self.theoryUI.currentTheory.update()
                 self.theoryUI.currentTheory.plotCurves()
-    ##################### Key pressed #####################
+        if event.modifiers() == Qt.ControlModifier and event.key() == Qt.Key_V:
+            clipboard = QApplication.clipboard()
+            pasted_text = clipboard.text()
+            self.pasteExpFromTPSCalc(pasted_text)
+            # print(f'Pasted text: {pasted_text}')
+
+    def pasteExpFromTPSCalc(self, pasted_text):
+        for table in self.tables:
+            table.newSpectraAdded = False
+        newSpectra = parse_asc_data(pasted_text)
+        if len(newSpectra) == 0:
+            return
+        self.experimentUI.addNewTable(newSpectra[0].fileType, newSpectra, "Pasted")
+        self.statusbar.showMessage("Paste")
+        for table in self.tables:
+            if table.newSpectraAdded:
+                table.fillTable()
+    ################# KEY PRESSED #################
 
 class TabBar(QTabBar):
     def tabSizeHint(self, index):
@@ -410,14 +276,15 @@ class TabBar(QTabBar):
 
 # main
 # QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True) #enable highdpi scaling
-QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True) #use highdpi icons
+# QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True) #use highdpi icons
 app = QApplication(sys.argv)
+# app.setPalette(DarkPalette())
 mainWindow = MainWindow()
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(mainWindow)
-widget.setMinimumWidth(int(screenSize()[0] * 0.4))
-widget.setMinimumHeight(int(screenSize()[1] * 0.4))
-widget.showMaximized()
+widget.setMinimumWidth(int(screenSize()[0] * 0.7))
+widget.setMinimumHeight(int(screenSize()[1] * 0.7))
+# widget.showMaximized()
 widget.show()
 try:
     sys.exit(app.exec_())
